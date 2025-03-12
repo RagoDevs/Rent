be530f2d1cb39624b813d25470178df6099a21ba
Jerry John <148745725+RagoTheDev@users.noreply.github.com>
2025-03-12 22:05:11 +0300

    Update page.jsx

--- src/app/Login/page.jsx
+++ src/app/Login/page.jsx
@@ -8,6 +8,7 @@ import { useAuth } from '@/Components/Require/AuthProvider';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useRouter } from 'next/navigation';
+import Link from "next/link";
 
 export default function Login() {
 
@@ -69,10 +70,6 @@ export default function Login() {
         },
     });
 
-    const handleShowForgotPassword = () => {
-        router.push('/forgotpassword')
-    }
-
     return (
         <>
             <ToastContainer />
@@ -117,8 +114,9 @@ export default function Login() {
                     {loginError && <div className="login-error">{loginError}</div>}
                     <button className="btn" type="submit">Login</button>
                     <div className="links">
-                        <p onClick={handleShowForgotPassword}>Forgot Password?</p> |
-                        <p>Create Account</p>
+                        <Link href="/forgotpassword">
+                            Forgot Password?
+                        </Link | <p>Create Account</p>
                     </div>
                 </form>
             </div>
