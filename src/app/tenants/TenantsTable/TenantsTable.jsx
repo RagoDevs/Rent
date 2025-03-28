68a39b2957aa357fcc784796ff32d089879aed2b
jerry <rago.d.e.v123@gmail.com>
2025-03-28 10:51:00 +0300

    added tenants api

--- src/app/tenants/TenantsTable/TenantsTable.jsx
+++ src/app/tenants/TenantsTable/TenantsTable.jsx
@@ -1,8 +1,39 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import './TenantsTable.css'
+import { useRouter } from 'next/navigation';
+import { base_url, checkExpiry, getToken } from '@/Components/constant';
 
 export default function TenantsTable() {
+    const [tenants, setTenants] = useState([])
+    const router = useRouter();
+
+    useEffect(() => {
+        if (checkExpiry()) {
+            router.push('/login')
+        }
+    }, [checkExpiry])
+
+    useEffect(() => {
+        const fetchTenants = async () => {
+            try {
+                const response = await fetch(`${base_url}/v1/auth/tenants`, {
+                    headers: {
+                        'Authorization': `Bearer ${getToken()}`,
+                        'Content-Type': 'application/json'
+                    }
+                })
+                const data = await response.json()
+                setTenants(data)
+            } catch (error) {
+                console.log('error occured')
+            }
+        }
+        if (!checkExpiry()) {
+            fetchTenants()
+        }
+    }, [checkExpiry, getToken])
     return (
         <>
             <div className="tenantcontainer--two">
@@ -13,53 +44,33 @@ export default function TenantsTable() {
                             <tr>
                                 <th>Name</th>
                                 <th>House</th>
-                                <th>Entry Date</th>
+                                <th>Present</th>
                                 <th>Contacts</th>
                             </tr>
                         </thead>
                         <tbody>
-                        <tr>
-                            <td>Jack Adam</td>
-                            <td>House A</td>
-                            <td>12.02.2023</td>
-                            <td>0712345678</td>
-                        </tr>
-                        <tr>
-                            <td>Jack Adam</td>
-                            <td>House A</td>
-                            <td>12.02.2023</td>
-                            <td>0712345678</td>
-                        </tr>
-                        <tr>
-                            <td>Jack Adam</td>
-                            <td>House A</td>
-                            <td>12.02.2023</td>
-                            <td>0712345678</td>
-                        </tr>
-                        <tr>
-                            <td>Jack Adam</td>
-                            <td>House A</td>
-                            <td>12.02.2023</td>
-                            <td>0712345678</td>
-                        </tr>
-                        <tr>
-                            <td>Jack Adam</td>
-                            <td>House A</td>
-                            <td>12.02.2023</td>
-                            <td>0712345678</td>
-                        </tr>
-                        <tr>
-                            <td>Jack Adam</td>
-                            <td>House A</td>
-                            <td>12.02.2023</td>
-                            <td>0712345678</td>
-                        </tr>
-                        <tr>
-                            <td>Jack Adam</td>
-                            <td>House A</td>
-                            <td>12.02.2023</td>
-                            <td>0712345678</td>
-                        </tr>
+                            {tenants && tenants.length > 0 ? (
+                                tenants.map((items, index) => {
+                                    return (
+                                        <tr key={index}>
+                                            <td>{items.name}</td>
+                                            <td>{ }</td>
+                                            <td style={{ color: items.active ? '#33ff3c' : '#ef091a' }}>
+                                                {items.active ? 'Present' : 'Absent'}
+                                            </td>
+                                            <td>{items.phone}</td>
+                                        </tr>
+                                    )
+                                })
+                            ) : (
+                                <tr>
+                                    <td colSpan={4} style={{ borderBottom: 'none' }}>
+                                        <div style={{ textAlign: 'center', verticalAlign: 'middle', height: '100px' }}>
+                                            No Data Available
+                                        </div>
+                                    </td>
+                                </tr>
+                            )}
                         </tbody>
                     </table>
                 </div>
