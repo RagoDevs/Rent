fe453569b9f5e501b3673b4b5ef5ade0a8f75b6a
jerry <rago.d.e.v123@gmail.com>
2025-04-01 11:06:51 +0300

    added inputs for adding house

--- src/Components/constant.js
+++ src/Components/constant.js
@@ -1,50 +1,73 @@
-export const base_url = 'https://rentbe.ragodevs.com'
+export const base_url = "https://rentbe.ragodevs.com";
 
-export const getToken = () => localStorage.getItem('rentSiteToken');
-export const getExpiry = () => localStorage.getItem('rentSiteExpiry');
+export const getToken = () => localStorage.getItem("rentSiteToken");
+export const getExpiry = () => localStorage.getItem("rentSiteExpiry");
 
 export const checkExpiry = () => {
-    const expiry = getExpiry();
-    if (expiry && Date.now() / 1000 > expiry) {
-        return true;
-    }
-    return false;
+  const expiry = getExpiry();
+  if (expiry && Date.now() / 1000 > expiry) {
+    return true;
+  }
+  return false;
 };
 
 export const fetchData = async (endpoint) => {
-    try {
-        const response = await fetch(`${base_url}${endpoint}`, {
-            headers: {
-                'Authorization': `Bearer ${getToken()}`,
-                'Content-Type': 'application/json'
-            }
-        });
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.log('error occured with const fetchdata')
-        return[];
+  try {
+    const response = await fetch(`${base_url}${endpoint}`, {
+      headers: {
+        Authorization: `Bearer ${getToken()}`,
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log("error occured with const fetchdata");
+    return [];
+  }
+};
+
+export const submitRequest = async (endpoint, method = "POST", body = null) => {
+  try {
+    const response = await fetch(`${base_url}${endpoint}`, {
+      method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: body ? JSON.stringify(body) : null,
+    });
+    const res = await response.json();
+
+    if (response.status < 200 || response.status >= 300) {
+      throw new Error(res.error || "Request failed from constant");
     }
-}
 
-export const submitRequest = async (endpoint, method = 'POST', body = null) => {
-    try {
-        const response = await fetch(`${base_url}${endpoint}`, {
-            method, 
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: body ? JSON.stringify(body) : null
-        });
-        const res = await response.json();
+    return res;
+  } catch (error) {
+    console.error("request error", error);
+    throw error;
+  }
+};
 
-        if (response.status < 200 || response.status >= 300) {
-            throw new Error(res.error || 'Request failed from constant')
-        }
+export const submitData = async (endpoint, method = "POST", body = null) => {
+  try {
+    const response = await fetch(`${base_url}${endpoint}`, {
+      method,
+      headers: {
+        Authorization: `Bearer ${getToken()}`,
+        "Content-Type": "application/json",
+      },
+      body: body ? JSON.stringify(body) : null,
+    });
+    const res = await response.json();
 
-        return res;
-    } catch (error) {
-        console.error('request error', error);
-        throw error
+    if (response.status < 200 || response.status >= 300) {
+      throw new Error(res.error || "Request failed from constant");
     }
-}
+
+    return res;
+  } catch (error) {
+    console.error("request error", error);
+    throw error;
+  }
+};
--- src/app/houses/Components/HouseTable/HousesTable.jsx
+++ src/app/houses/Components/HouseTable/HousesTable.jsx
@@ -5,9 +5,24 @@ import './HouseTable.css';
 
 export default function HousesTable({ houses = [], setSelectedHouse }) {
     const [searchQuery, setSearchQuery] = useState('')
+    const [modal, setModal] = useState(false);
+    const [form, setForm] = useState({
+        location: '',
+        block: '',
+        partition: '',
+        occupied: ''
+    })
+
 
     const filterHouses = houses.filter((house) =>
-    house.location.toLowerCase().includes(searchQuery.toLowerCase()))
+        house.location.toLowerCase().includes(searchQuery.toLowerCase()))
+
+    function handleChange(e) {
+        setForm({
+            ...form, 
+            [e.target.name]: e.target.value,
+        });
+    }
     return (
         <>
             <div className="houses-table-container">
@@ -15,19 +30,67 @@ export default function HousesTable({ houses = [], setSelectedHouse }) {
                     <h2>Houses</h2>
                     <div className="househeader--search">
                         <div className="search--input">
-                            <input 
-                            type="search" 
-                            value={searchQuery}
-                            placeholder="Search House" 
-                            onChange={(e) => setSearchQuery(e.target.value)}
+                            <input
+                                type="search"
+                                value={searchQuery}
+                                placeholder="Search House"
+                                onChange={(e) => setSearchQuery(e.target.value)}
                             />
 
                         </div>
-                        <div className="add--house">
-                            <button>New House</button>
+                        <div className="add-house-btn">
+                            <button onClick={() => setModal(true)}>New House</button>
                         </div>
                     </div>
                 </div>
+                {modal && (
+                    <div className="add-house-bg">
+                        <div className="add-house">
+                            <button className="close-btn" onClick={() => setModal(false)}>
+                                ✖
+                            </button>
+                            <div className="add-house-form">
+                                <h4>ADD A NEW HOUSE</h4>
+                                <form>
+                                    <input
+                                        type="text"
+                                        name='location'
+                                        placeholder='Add Location'
+                                        value={form.location}
+                                        onChange={handleChange}
+                                        required
+                                    />
+                                    <input
+                                        type="text"
+                                        name='block'
+                                        placeholder='Add Block'
+                                        value={form.block}
+                                        onChange={handleChange}
+                                        required
+                                    />
+                                    <input
+                                        type="text"
+                                        name='partition'
+                                        placeholder='Add Partition'
+                                        value={form.partition}
+                                        onChange={handleChange}
+                                        required
+                                    />
+                                    <div className="add-house-dropdown">
+                                        <label>Is it Occupied?</label>
+                                        <select name="occupied" value={form.occupied} onChange={handleChange}>
+                                            <option value="" disabled>Pick Options</option>
+                                            <option value={true}>Occupied</option>
+                                            <option value={false}>Vacant</option>
+                                        </select>
+
+                                    </div>
+                                    <button>Send It</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                )}
                 <div className="house--table">
 
                     <table>
@@ -43,17 +106,17 @@ export default function HousesTable({ houses = [], setSelectedHouse }) {
                             {filterHouses.length > 0 ? (
                                 filterHouses.map((items, index) => {
                                     return (
-                                    <tr 
-                                    key={index} 
-                                    onClick={() => setSelectedHouse(items.id)}
-                                    style={{cursor: 'pointer'}}
-                                    >
-                                        <td>{items.location}</td>
-                                        <td>{items.block}</td>
-                                        <td>{items.partition}</td>
-                                        <td style={{ color: items.occupied ? '#33ff3c' : '#ef091a' }}>
-                                            {items.occupied ? 'Occupied' : 'Vacant'}</td>
-                                    </tr>
+                                        <tr
+                                            key={index}
+                                            onClick={() => setSelectedHouse(items.id)}
+                                            style={{ cursor: 'pointer' }}
+                                        >
+                                            <td>{items.location}</td>
+                                            <td>{items.block}</td>
+                                            <td>{items.partition}</td>
+                                            <td style={{ color: items.occupied ? '#33ff3c' : '#ef091a' }}>
+                                                {items.occupied ? 'Occupied' : 'Vacant'}</td>
+                                        </tr>
                                     );
                                 })
                             ) : (
@@ -69,6 +132,7 @@ export default function HousesTable({ houses = [], setSelectedHouse }) {
                     </table>
                 </div>
             </div>
+
         </>
     )
 }
